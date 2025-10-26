b2a06bac4efb0021e724ddaf633cde8cac77d946
atmetz <atmetz@gmail.com>
2025-10-26 12:17:02 -0500

    prototype board space

---
+++ pylieff.py
@@ -0,0 +1,8 @@
+from ui import ui
+
+def main():
+    print("Running Pylieff!")
+    ui()
+
+if __name__ == "__main__":
+    main()
---
+++ ui.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from functools import partial
+
+def on_button_click(button, root):
+    x_coord = button.winfo_x()
+    y_coord = button.winfo_y()
+    width = button.winfo_width()
+    height = button.winfo_height()
+    button.place_forget()
+
+    label_text = Label(root, bg="blue")
+    label_text.place(x = x_coord, y = y_coord, width = width, height = height)
+
+def ui():
+    root = Tk()
+
+    root.title("Pylieff")
+    root.geometry("800x600")
+
+    button = []
+
+    for i in range(16):
+        button.append(Button(root, activebackground="blue", activeforeground="white"))
+        
+    start_x = 300
+    start_y = 150
+    board_x = 0
+    board_y = 0
+    b_width = 50
+    b_height = 50
+
+    for i in range(16):
+        button[i].place(x = start_x + board_x, y = start_y + board_y, width = b_width, height = b_height)
+        button[i].config(command=partial(on_button_click, button[i], root))
+        board_x += 50
+        if board_x > 150:
+            board_y += 50
+            board_x = 0
+
+    root.mainloop()
